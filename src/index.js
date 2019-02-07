65befd8e80113e1b015fb883de5cb705f82ed974
horangijk <iamjooyoung@gmail.com>
2019-02-06 23:35:49 -0500

    Edit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+  const dogBar = document.querySelector('#dog-bar')
+  let span;
+  const dogInfo = document.querySelector('#dog-info')
+  fetch('http://localhost:3000/pups')
+  .then(res => res.json())
+  .then(pups => {
+    pups.forEach(pup => {
+      dogBar.innerHTML += `<span class="dog-button" data-id="${pup.id}">${pup.name}</span>`
+
+
+    })
+  })
+  dogBar.addEventListener('click', e => {
+
+    if(e.target.classList.contains('dog-button')){
+    fetch(`http://localhost:3000/pups/${e.target.dataset.id}`)
+      .then(res => res.json())
+      .then(pup => {
+        const dogInfo = document.querySelector('#dog-info')
+        // const img = document.createElement('img')
+        // const h2 = document.createElement('h2')
+
+        // dogButton.className = 'dog-button'
+        // img.src = pup.image
+        // h2.innerText = pup.name
+        // if (pup.isGoodDog) {
+        // dogButton.innerText = 'Good Dog!'
+        // } else {
+        // dogButton.innerText = 'Bad Dog!'
+        // }
+        // // console.log(dogButton)
+        // dogInfo.append(img)
+        // dogInfo.append(h2)
+        // dogInfo.append(dogButton)
+        dogInfo.innerHTML = `<img src=${pup.image}>
+        <h2>${pup.name}</h2>
+        <button class="goodBoy"></button>`
+        const dogButton = dogInfo.querySelector('.goodBoy')
+
+        if (pup.isGoodDog) {
+        dogButton.innerText = 'Good Dog!'
+        } else {
+        dogButton.innerText = 'Bad Dog!'
+        }
+      })
+    }
+
+
+  })
+  //text should change from Good to Bad or Bad to Good and be updated to the database
+  //(pessimistically render)
+  dogInfo.addEventListener('click', e => {
+    // console.log(e.target)
+    fetch(`http://localhost:3000/pups/${e.target.dataset.id}`, {
+      method: 'PATCH',
+      header: {
+        'Content-Type': 'application/json'
+      }
+      body: JSON.stringify(
+        {
+        isGoodDog: !pup.isGoodDog
+      })
+    })
+    .then(res => res.json())
+    .then(pup => {
+      if (pup.isGoodDog) {
+      dogButton.innerText = 'Bad Dog!'
+      } else {
+      dogButton.innerText = 'Good Dog!'
+      }
+    })
+
+
+  })
+})//DOMContentLoaded
